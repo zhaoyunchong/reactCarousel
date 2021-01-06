aa59a7a74b75010a80f4fb4deb55e319b1591a13
zhaoyunchong <2635599463@qq.com>
2021-01-06 11:33:12 +0800

    add

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ console.log('hello world');
 class App extends Component {
   render(){
     console.log('你好吗');
+    console.log('this is a react app!');
     return (
       <div className="app">
         <Tab tabJson={{picUrl:['1.jpg','2.jpg','3.jpg','4.jpg'],timer:2000}} />
