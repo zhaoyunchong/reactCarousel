cbcdeb8bad2eef598312ca9b9d515dcd8216dd00
zhaoyunchong <2635599463@qq.com>
2021-01-06 10:41:07 +0800

    111

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React,{ Component } from 'react'
 import ReactDOM from 'react-dom'
 import "./index.scss"
 import Tab from "./tab"
+console.log('hello world');
 class App extends Component {
   render(){
+    console.log('你好吗');
     return (
       <div className="app">
         <Tab tabJson={{picUrl:['1.jpg','2.jpg','3.jpg','4.jpg'],timer:2000}} />
